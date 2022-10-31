edf3fa7556d45721c77f941dbe5c3cb5e1a82dfb
taijusanagi <taijusanagi@gmail.com>
2022-11-01 02:03:10 +0900

    fix: contract errro

--- packages/app/lib/contract.ts
+++ packages/app/lib/contract.ts
@@ -1,2 +1,2 @@
-export const equipment = "0xeeeb47597825661a2077c6280a705b90f8749078";
-export const material = "0x23e8330bf482c894b2a257c8bb07d1d527ba5a8b";
+export const equipmentAddress = "0xeeeb47597825661a2077c6280a705b90f8749078";
+export const materialAddress = "0x23e8330bf482c894b2a257c8bb07d1d527ba5a8b";
--- packages/app/pages/index.tsx
+++ packages/app/pages/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import { Box, Container, Flex, HStack, Text, Stack, Button } from "@chakra-ui/react";
 import { ConnectButton } from "@rainbow-me/rainbowkit";
@@ -6,57 +6,131 @@ import { ConnectButton } from "@rainbow-me/rainbowkit";
 import dynamic from "next/dynamic";
 import p5Types from "p5";
 
+import { useAccount, useSigner } from "wagmi";
+
+import { equipmentABI } from "../lib/abi/equipment";
+import { ethers } from "ethers";
+import { equipmentAddress } from "../lib/contract";
+
 const Sketch = dynamic(import("react-p5"), {
   loading: () => <></>,
   ssr: false,
 });
 
-const setup = (p5: p5Types, canvasParentRef: Element) => {
-  p5.createCanvas(p5.windowWidth, p5.windowHeight / 1.5).parent(canvasParentRef);
-  p5.strokeWeight(2);
-  p5.ellipseMode(p5.RADIUS);
-};
-
-const draw = (p5: p5Types) => {
-  let c1 = p5.color(255, 204, 0);
-  let c2 = p5.color(20, 60, 0);
-  let c3 = p5.color(43, 250, 70);
-
-  p5.background(204);
-
-  // Neck
-  p5.stroke(c3); // Set stroke to gray
-  p5.line(266, 257, 266, 162); // Left
-  p5.line(276, 257, 276, 162); // Middle
-  p5.line(286, 257, 286, 162); // Right
-
-  // Antennae
-  p5.line(276, 155, 246, 112); // Small
-  p5.line(276, 155, 306, 56); // Tall
-  p5.line(276, 155, 342, 170); // Medium
-
-  // Body
-  p5.noStroke(); // Disable stroke
-  p5.fill(c1); // Set fill to gray
-  p5.ellipse(264, 377, 33, 33); // Antigravity orb
-  p5.fill(c2); // Set fill to black
-  p5.rect(219, 257, 90, 120); // Main body
-  p5.fill(c1); // Set fill to gray
-  p5.rect(219, 274, 90, 6); // Gray stripe
-
-  p5.fill(c2); // Set fill to black
-  p5.ellipse(276, 155, 45, 45); // Head
-  p5.fill(255); // Set fill to white
-  p5.ellipse(288, 150, 14, 14); // Large eye
-  p5.fill(c2); // Set fill to black
-  p5.ellipse(288, 150, 3, 3); // Pupil
-  p5.fill(153); // Set fill to light gray
-  p5.ellipse(263, 148, 5, 5); // Small eye 1
-  p5.ellipse(296, 130, 4, 4); // Small eye 2
-  p5.ellipse(305, 162, 3, 3); // Small eye 3
-};
-
 export default function Home() {
+  const { address } = useAccount();
+  const { data: signer } = useSigner();
+
+  const [mode, setMode] = useState<"loading" | "none" | "select" | "move">("loading");
+  const [tokenIds, setTokenIds] = useState<string[]>([]);
+  const [selectedTokenId, setSelectedTokenId] = useState("");
+
+  const setup = (p5: p5Types, canvasParentRef: Element) => {
+    p5.createCanvas(p5.windowWidth, p5.windowHeight / 1.5).parent(canvasParentRef);
+    p5.strokeWeight(2);
+    p5.ellipseMode(p5.RADIUS);
+  };
+
+  const draw = (p5: p5Types) => {
+    let c1 = p5.color(255, 204, 0);
+    let c2 = p5.color(20, 60, 0);
+    let c3 = p5.color(43, 250, 70);
+
+    p5.background(204);
+
+    // Neck
+    p5.stroke(c3); // Set stroke to gray
+    p5.line(266, 257, 266, 162); // Left
+    p5.line(276, 257, 276, 162); // Middle
+    p5.line(286, 257, 286, 162); // Right
+
+    // Antennae
+    p5.line(276, 155, 246, 112); // Small
+    p5.line(276, 155, 306, 56); // Tall
+    p5.line(276, 155, 342, 170); // Medium
+
+    // Body
+    p5.noStroke(); // Disable stroke
+    p5.fill(c1); // Set fill to gray
+    p5.ellipse(264, 377, 33, 33); // Antigravity orb
+    p5.fill(c2); // Set fill to black
+    p5.rect(219, 257, 90, 120); // Main body
+    p5.fill(c1); // Set fill to gray
+    p5.rect(219, 274, 90, 6); // Gray stripe
+
+    p5.fill(c2); // Set fill to black
+    p5.ellipse(276, 155, 45, 45); // Head
+    p5.fill(255); // Set fill to white
+    p5.ellipse(288, 150, 14, 14); // Large eye
+    p5.fill(c2); // Set fill to black
+    p5.ellipse(288, 150, 3, 3); // Pupil
+    p5.fill(153); // Set fill to light gray
+    p5.ellipse(263, 148, 5, 5); // Small eye 1
+    p5.ellipse(296, 130, 4, 4); // Small eye 2
+    p5.ellipse(305, 162, 3, 3); // Small eye 3
+  };
+
+  const mint = async () => {
+    if (!signer) {
+      return;
+    }
+    const contract = new ethers.Contract(equipmentAddress, equipmentABI, signer);
+    const value = ethers.utils.parseEther("0.01");
+    await contract.mint({ value });
+  };
+
+  const selectEquipment = (tokenId: string) => {
+    setSelectedTokenId(tokenId);
+    setMode("move");
+  };
+
+  const move = async (dir: number) => {
+    if (!signer) {
+      return;
+    }
+
+    console.log(selectedTokenId.toString());
+    const contract = new ethers.Contract(equipmentAddress, equipmentABI, signer);
+    await contract.move(selectedTokenId, dir);
+    setMode("move");
+  };
+
+  useEffect(() => {
+    const provider = new ethers.providers.JsonRpcProvider("https://api.testnet-dev.trust.one/");
+    const contract = new ethers.Contract(equipmentAddress, equipmentABI, provider);
+
+    contract.ownerOf(1).then((owner) => {
+      console.log("owner", owner);
+    });
+
+    const filter = contract.filters.Mined();
+    contract.queryFilter(filter).then((logs) => {
+      console.log(logs);
+      const tokenIds = Array.from(new Set(logs.map((log) => log.args!.tokenId)));
+      if (tokenIds.length === 0) {
+        setMode("none");
+      } else {
+        setTokenIds(tokenIds);
+        setMode("select");
+      }
+    });
+  }, [address]);
+
+  useEffect(() => {
+    if (mode !== "move") {
+      return;
+    }
+
+    const provider = new ethers.providers.JsonRpcProvider("https://api.testnet-dev.trust.one/");
+    const contract = new ethers.Contract(equipmentAddress, equipmentABI, provider);
+
+    contract.locations(selectedTokenId).then((location: any) => {
+      console.log("current location...");
+      console.log("x", location.x.toString());
+      console.log("y", location.x.toString());
+    });
+  }, [mode]);
+
   return (
     <Flex minHeight={"100vh"} direction={"column"}>
       <Container as="section" maxW="8xl" mb="8">
@@ -71,22 +145,86 @@ export default function Home() {
           </Flex>
         </Box>
       </Container>
-      <Stack mb="4">
-        <Sketch setup={setup} draw={draw} />
-      </Stack>
-      <Container maxW="2xl" mb="4">
-        <Box py="4" px="4" boxShadow={"base"} borderRadius="2xl" bgColor={"white"}>
-          <Text fontSize="xl" fontWeight={"bold"} mb="4" textAlign="center">
-            Direction
-          </Text>
-          <HStack>
-            <Button w="full">Back</Button>
-            <Button w="full">Left</Button>
-            <Button w="full">Right</Button>
-            <Button w="full">Front</Button>
-          </HStack>
-        </Box>
-      </Container>
+      {mode === "loading" && (
+        <>
+          {/* <Stack mb="4">
+            <Sketch setup={setup} draw={draw} />
+          </Stack> */}
+          <Container maxW="2xl" mb="4">
+            <Box py="4" px="4" boxShadow={"base"} borderRadius="2xl" bgColor={"white"}>
+              <Text fontSize="xl" fontWeight={"bold"} textAlign="center">
+                Loading...
+              </Text>
+            </Box>
+          </Container>
+        </>
+      )}
+      {mode === "none" && (
+        <>
+          <Container maxW="2xl" mb="4">
+            <Box py="4" px="4" boxShadow={"base"} borderRadius="2xl" bgColor={"white"}>
+              <Stack>
+                <Text fontSize="xl" fontWeight={"bold"} textAlign="center" mb="4">
+                  You don&apos;t have equipment yet
+                </Text>
+                <Button onClick={mint}>Mint (0.01 ETH)</Button>
+              </Stack>
+            </Box>
+          </Container>
+        </>
+      )}
+      {mode === "select" && (
+        <>
+          <Container maxW="2xl" mb="4">
+            <Box py="4" px="4" boxShadow={"base"} borderRadius="2xl" bgColor={"white"}>
+              <Stack>
+                {tokenIds.map((tokenId) => {
+                  return (
+                    <Button
+                      key={tokenId}
+                      boxShadow={"base"}
+                      borderRadius="2xl"
+                      onClick={() => selectEquipment(tokenId)}
+                    >
+                      TokenID: {tokenId.toString()}
+                    </Button>
+                  );
+                })}
+                <Button onClick={mint}>Mint (0.01 ETH)</Button>
+              </Stack>
+            </Box>
+          </Container>
+        </>
+      )}
+
+      {mode === "move" && (
+        <>
+          <Stack mb="4">
+            <Sketch setup={setup} draw={draw} />
+          </Stack>
+          <Container maxW="2xl" mb="4">
+            <Box py="4" px="4" boxShadow={"base"} borderRadius="2xl" bgColor={"white"}>
+              <Text fontSize="xl" fontWeight={"bold"} mb="4" textAlign="center">
+                Direction
+              </Text>
+              <HStack>
+                <Button w="full" onClick={() => move(3)}>
+                  Back
+                </Button>
+                <Button w="full" onClick={() => move(1)}>
+                  Left
+                </Button>
+                <Button w="full" onClick={() => move(0)}>
+                  Right
+                </Button>
+                <Button w="full" onClick={() => move(2)}>
+                  Front
+                </Button>
+              </HStack>
+            </Box>
+          </Container>
+        </>
+      )}
     </Flex>
   );
 }
